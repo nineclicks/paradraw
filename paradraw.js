59f8c4f088a239f806708e7930280dc08b20cfad
nineclicks <nicholasg1989@gmail.com>
2017-11-08 16:23:01 -0800

    vector functions, wheel pan/zoom and zoom follow cursor

--- paradraw.js
+++ paradraw.js
@@ -8,10 +8,17 @@ var subDiv      = 5;
 var gridDivider = 1.0;
 var gridDark    = "#99f";
 var gridLight   = "#99f";
+var unit        = 10;
 
 var axisWeight = 3;
 var gridWeight = 1.5;
 var subWeight  = 0.5;
+
+var getTranslatedCoords = function(x, y) {
+  var x = (x - gx) / unit * zoom;  
+  var y = (y - gy) / unit * zoom;  
+  return [x, -y];
+}
 
 var mouse = {
   "mode" :   "none",
@@ -22,17 +29,16 @@ var mouse = {
 var resizeCanvas = function() {
   canvas[0].width = window.innerWidth - 10;
   canvas[0].height = window.innerHeight - 10;
-  mainCtx.clear();
   drawGrid(mainCtx);
 }
 
 var drawGrid = function(ctx) {
+  ctx.clear();
   var gridDivPx = gridSpacing / subDiv / zoom * gridDivider;
   var xOffset = gx % gridDivPx;
   var xStart  = Math.ceil(-gx / gridDivPx);
   var yOffset = gy % gridDivPx;
   var yStart  = Math.ceil(-gy / gridDivPx);
-  console.log(xOffset);
 
   for (var i = 0; i * gridDivPx < canvas[0].width; i++) {
     var div = xStart + i;
@@ -63,43 +69,115 @@ var drawGrid = function(ctx) {
   }
 }
 
-var canvasMouse = function(event) {
+var canvasMouse = function(e) {
+  console.log(getTranslatedCoords(e.pageX, e.pageY));
+  if (e.buttons == 0) {
+    canvasUp();
+    return;
+  }
   if (mouse.mode == "scroll") {
-    gx = event.pageX - mouse.startX;
-    gy = event.pageY - mouse.startY;
-    mainCtx.clear();
+    gx = e.pageX - mouse.startX;
+    gy = e.pageY - mouse.startY;
     drawGrid(mainCtx);
   } else if (mouse.mode == "zoom") {
-    var zChange = mouse.startY - event.pageY;
-    mouse.startY = event.pageY;
+    var zChange = mouse.startY - e.pageY;
+    mouse.startY = e.pageY;
     changeZoom(zChange);
   }
 }
 
-var changeZoom = function(change) {
+var vAdd = function(v1, v2) {
+  out = [];
+  for (var i = 0; i < v1.length || i < v2.length; i++) {
+    if (i >= v1.length) v1[i] = 0;
+    if (i >= v2.length) v2[i] = 0;
+    out[i] = v1[i] + v2[i]; 
+  }
+  return out;
+}
+
+var vSub = function(v1, v2) {
+  out = [];
+  for (var i = 0; i < v1.length || i < v2.length; i++) {
+    if (i >= v1.length) v1[i] = 0;
+    if (i >= v2.length) v2[i] = 0;
+    out[i] = v1[i] - v2[i]; 
+  }
+  return out;
+}
+
+var vMult = function(v1, v2) {
+  out = [];
+  if (Array.isArray(v2)) {
+    for (var i = 0; i < v1.length || i < v2.length; i++) {
+      if (i >= v1.length) v1[i] = 0;
+      if (i >= v2.length) v2[i] = 0;
+      out[i] = v1[i] * v2[i]; 
+    }
+  } else {
+    for (var i = 0; i < v1.length; i++) {
+      out[i] = v1[i] * v2; 
+    }
+  }
+  return out;
+}
+
+var vDiv = function(v1, v2) {
+  out = [];
+  if (Array.isArray(v2)) {
+    for (var i = 0; i < v1.length || i < v2.length; i++) {
+      if (i >= v1.length) v1[i] = 0;
+      if (i >= v2.length) v2[i] = 0;
+      out[i] = v1[i] / v2[i]; 
+    }
+  } else {
+    for (var i = 0; i < v1.length; i++) {
+      out[i] = v1[i] / v2; 
+    }
+  }
+  return out;
+}
+
+console.log(vDiv([2,4,6],2));
+
+var changeZoom = function(change, x, y) {
+  if (x == null || y == null) {
+    x = 0;
+    y = 0;
+  }
+  currentGraphCoords = getTranslatedCoords(x, y);
+  currentPixelCoords = [x, y];
   zoom *= (1 + (change / 100.0));
   gridDivider = 1.0;
   while (gridSpacing / subDiv / zoom * gridDivider < 10.0) {
     gridDivider *= 10;
   }
-  console.log(gridDivider);
-  mainCtx.clear();
+  newGraphCoords = getTranslatedCoords(x, y);
+  graphCoordDiff = vSub(currentGraphCoords, newGraphCoords);
+  graphCoordDiffScaled = vMult(graphCoordDiff, unit / zoom);
+  gx = gx - graphCoordDiffScaled[0];
+  gy = gy + graphCoordDiffScaled[1];
   drawGrid(mainCtx);
 }
 
-var canvasDown = function(event) {
-  if (event.ctrlKey) {
-    mouse.startX = event.pageX - gx;
-    mouse.startY = event.pageY - gy;
+var canvasDown = function(e) {
+  if (e.which == 2 || e.button == 4 || e.ctrlKey) {
+    mouse.startX = e.pageX - gx;
+    mouse.startY = e.pageY - gy;
     mouse.mode = "scroll";
-  } else if (event.shiftKey) {
-    mouse.startY = event.pageY;
+  } else if (e.shiftKey) {
+    mouse.startY = e.pageY;
     mouse.mode = "zoom";
   }
 }
 
-var canvasUp = function(event) {
+var canvasUp = function(e) {
   mouse.mode = "none";
+}
+
+var wheel = function(e) {
+  changeZoom(-e.wheelDelta / 10.0, e.x, e.y);
+
 }
 
 var injectCanvas = function() {
@@ -107,6 +185,7 @@ var injectCanvas = function() {
   canvas.mousemove(canvasMouse);
   canvas.mousedown(canvasDown);
   canvas.mouseup(canvasUp);
+  canvas[0].addEventListener('mousewheel', wheel, false);
   mainCtx = canvas[0].getContext("2d");
   resizeCanvas();
   gx = canvas[0].width  / 2;
